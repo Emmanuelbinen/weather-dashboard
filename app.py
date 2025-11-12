da7d064ec00762c893094752bc10557023b80b8e
Emmanuel Binen <emmanuelbinen@outlook.com>
2025-11-12 11:07:19 +0200

    In this changes, we: -We create a new route /weather that handles form submissions -We get the city name from the form -We make an API request to OpenWeatherMap -We extract the weather data and send it to the template -We handle errors if the city isn't found

--- app.py
+++ app.py
@@ -10,5 +10,38 @@ API_KEY = "de2d5eeb5277d4a6e3fe79fb9d6c67ea"
 def index():
     return render_template('index.html')
 
+@app.route('/weather', methods=['POST'])
+def weather():
+    city = request.form.get('city')
+    
+    # OpenWeatherMap API URL
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    
+    try:
+        # Make the API request
+        response = requests.get(url)
+        data = response.json()
+        
+        # Check if city was found
+        if response.status_code == 200:
+            weather_data = {
+                'city': data['name'],
+                'country': data['sys']['country'],
+                'temperature': round(data['main']['temp']),
+                'description': data['weather'][0]['description'].capitalize(),
+                'humidity': data['main']['humidity'],
+                'wind_speed': data['wind']['speed'],
+                'feels_like': round(data['main']['feels_like']),
+                'pressure': data['main']['pressure']
+            }
+            return render_template('index.html', weather=weather_data)
+        else:
+            error = "City not found. Please try again."
+            return render_template('index.html', error=error)
+            
+    except Exception as e:
+        error = "Something went wrong. Please try again."
+        return render_template('index.html', error=error)
+
 if __name__ == '__main__':
     app.run(debug=True)
